9b30f0fad7169a1b6873e4551ef01fbea37bd39a
lordofall <akhilesh4u3@gmail.com>
2021-03-22 16:49:29 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,7 +2,7 @@
 
 def call(body) {
   
-  def config = [:]
+  /*def config = [:]
   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = config
   body()
@@ -11,7 +11,23 @@ def call(body) {
   def num2   = config.NUM2
   def name = config.NAME
   echo "sum "+num1+num2
-  echo "Hello, ${name}."
+  echo "Hello, ${name}." */
+  
+  node {
+    def mvnHome
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def num1 = config.NUM1
+    def num2   = config.NUM2
+    def name = config.NAME
+    stage('Preparation') { // for display purposes
+        echo "sum "+num1+num2
+        echo "Hello, ${name}."
+    }
+  }
   
 
 }
