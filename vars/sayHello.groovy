19611ac0c2043fda6636cfd2f94e6c2c20057776
lordofall <akhilesh4u3@gmail.com>
2021-03-22 14:57:44 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,10 +1,17 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
+def call(body) {
+  
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  
+  def num1 = config.NUM1
+  def num2   = config.NUM2
+  echo "sum "+num1+num2
   echo "Hello, ${name}."
   
-  def num1           = config.NUM1
-  def num2           = config.NUM2
-  echo "sum "+num1+num2
+
 }
 
