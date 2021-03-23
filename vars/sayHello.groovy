2f2549a7ad8faa229027fa4ec8f92e392280e7a7
akhilesh4u3 <akhilesh4u3@gmail.com>
2021-03-23 12:23:07 +0530

    mail related change

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -45,14 +45,12 @@ def call(body) {
                 echo "Hello, ${name}."
             }
         }
-      stage('sendmail') {
          post {
           always {
             emailext body: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS: Check console output at $BUILD_URL to view the results.', 
             recipientProviders: [developers(), requestor()], subject: 'test subject', to: 'test@jenkins'
           }
         }
-      }
       
       
     }
