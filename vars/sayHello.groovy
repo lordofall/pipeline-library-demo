e51726b4cd7d7f6ae1bb15622feecacf8927484a
akhilesh4u3 <akhilesh4u3@gmail.com>
2021-03-23 13:32:42 +0530

    git show --name-only

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -36,6 +36,7 @@ def call(body) {
   def num1 = config.NUM1
   def num2   = config.NUM2
   def name = config.NAME
+  def currentCommit = $(git show --name-only)
   pipeline {
     agent any
     stages {
@@ -49,19 +50,15 @@ def call(body) {
 
     post {
       always {
+
+
          emailext body: """
          ${env.JOB_NAME} - Build # ${env.BUILD_NUMBER} - ${currentBuild.currentResult}<br/>
          Check console output at ${env.BUILD_URL} to view the results.<br/>
          Job name: ${env.JOB_NAME} <br/>
-         BRANCH_NAME: ${env.BRANCH_NAME} <br/>
-         CHANGE_ID: ${env.CHANGE_ID} <br/>
-         CHANGE_URL: ${env.CHANGE_URL} <br/>
-         CHANGE_TITLE: ${env.CHANGE_TITLE} <br/>
-         CHANGE_AUTHOR: ${env.CHANGE_AUTHOR} <br/>
-         CHANGE_AUTHOR_DISPLAY_NAME: ${env.CHANGE_AUTHOR_DISPLAY_NAME} <br/>
-         CHANGE_TARGET: ${env.CHANGE_TARGET} <br/>
-         CHANGE_BRANCH: ${env.CHANGE_BRANCH} <br/>
-         BUILD_NUMBER: ${env.BUILD_NUMBER} <br/>
+         env Current commit: ${env.currentCommit} <br/>
+         Current commit: ${currentCommit} <br/>
+         
          JOB_NAME: ${env.JOB_NAME} <br/>
          BUILD_URL: ${env.BUILD_URL} <br/>
          JOB_URL: ${env.JOB_URL} <br/>
